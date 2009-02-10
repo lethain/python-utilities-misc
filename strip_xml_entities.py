987907af62a9a0ceef073ccc0db4b8c7ac646fee
Will Larson <lethain@gmail.com>
2009-02-09 23:14:34 -0500

    Cleaned up strip_xml_entities.py

--- strip_xml_entities.py
+++ strip_xml_entities.py
@@ -7,18 +7,21 @@ reload(sys)
 sys.setdefaultencoding('utf_8')
 
 def strip_chars(f,extra=u''):
-    remove_re = re.compile(u'[\x00-\x08\x0B-\x0C\x0E-\x1F\x7F%s]' % extra)
+    remove_re = re.compile(u'[\x00-\x08\x0B-\x0C\x0E-\x1F\x7F%s]'
+                           % extra)
     i = 1
     stripped = 0
     for line in f:
         new_line, count = remove_re.subn('', line) 
         if count > 0:
             plur = ((count > 1) and u's') or u''
-            sys.stderr.write('Line %d, removed %s character%s.\n' % (i, count, plur))
+            sys.stderr.write('Line %d, removed %s character%s.\n'
+                             % (i, count, plur))
         sys.stdout.write(new_line)
         stripped = stripped + count
         i = i + 1
-    sys.stderr.write('Stripped %d characters from %d lines.\n' % (stripped, i))
+    sys.stderr.write('Stripped %d characters from %d lines.\n'
+                     % (stripped, i))
 
 def main():
     p = OptionParser("usage: strip_xml_entities.py file/to/parse.xml")
@@ -35,3 +38,4 @@ def main():
 
 if __name__ == '__main__':
     sys.exit(main())
+    
